0f537faa9d12de9ed647c27fde26574c2070be96
lixu04 <lixu04@longfor.com>
2022-04-03 18:40:47 +0800

    chore: target

--- multi-thread/target/debug/deps/multi_thread-f8c3a3881d83f0cb.d
+++
@@ -1,5 +0,0 @@
-/Users/a2020/Desktop/project/rust/rust-course/multi-thread/target/debug/deps/multi_thread-f8c3a3881d83f0cb: src/main.rs
-
-/Users/a2020/Desktop/project/rust/rust-course/multi-thread/target/debug/deps/multi_thread-f8c3a3881d83f0cb.d: src/main.rs
-
-src/main.rs:
--- multi-thread/target/debug/multi-thread.d
+++
@@ -1,1 +0,0 @@
-/Users/a2020/Desktop/project/rust/rust-course/multi-thread/target/debug/multi-thread: /Users/a2020/Desktop/project/rust/rust-course/multi-thread/src/main.rs
